189291b44d8ada91a0c3227f87702bec242c9037
marisahleleshep <leleshepmmarisaa@gmail.com>
2023-03-08 13:01:21 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,41 @@
+let x= "extravaganza"
+console.log(x[8],x[9],x[10],x[11]);
+
+
+const b="eat"
+// const food = "The quick fox jumped over the lazy dog"
+const food=`The quick fox jumped ${b} the lazy dog`
+console.log(food)
+
+
+const a ="the"
+const c ="brown"
+const story ="The  quick fox jumped on the lazy dog"
+const quick = (story.match(/the/g)||[]).length;
+const fox=(story.match(/brown/g)||[]).lenght;
+console.log(quick);
+
+const str ="are"
+const string1 = "The pupils are reading in the library";
+
+const huge="wonderful"
+const huger=huge.toUpperCase()
+console.log(huger)
+const less="amazing"
+const lesser=less.toLowerCase()
+console.assert(lesser)
+const tiny="BEneath"
+const tiner=tiny.toLowerCase()
+console.log(tiner)
+const title="A wonderful world"
+const title2=title.toLocaleUpperCase()
+console.log(title2)
+
+
+
+
+
+
+
+
+
